1fa07cad7c45fb9fc86bd6674489566557f322be
kopiadjie <jiperdanaksm@gmail.com>
2023-03-14 17:52:20 +0700

    ketiga

---
+++ Code/sesi4/factorials.py
@@ -0,0 +1,7 @@
+def factorials(n):
+    return 1 if n <= 1 else n * factorials(n-1)
+
+def penjumlahan(x,y,z):
+    return x**2
+
+if __name__ == '__Main__'
---
+++ Code/sesi4/mod.py
@@ -0,0 +1,7 @@
+s = "tutorial python kampus merdeka"
+mylist = [10,20,30,40]
+
+def fungsi_kostum(x,y,z):
+    return x**2 + y*z
+
+print
